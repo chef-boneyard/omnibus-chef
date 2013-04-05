chef/chef

commit 9f9cc08aa1d40d8d24287926b0e6958e7c09b219

build Erchef url off configured values for listen + port
The default attribute value for `node['chef_server']['erchef']['url']`
is out of date the instant a user configures alternate values for
`listen` or `port`. We'll remove this misleading attribute and just
compute a url when we need it using the following format:

  http://ERCHEF_LISTEN:ERCHEF_PORT

This fixes the following issue:

* [CHEF-3887] Cannot change port of erchef

files/chef-server-cookbooks/chef-server/recipes/chef-server-webui.rb
@@ -42,12 +42,17 @@ session_store_config = File.join(chef_server_webui_etc_dir, "session_store.rb")
 secret_token_config = File.join(chef_server_webui_etc_dir, "secret_token.rb")
 config_ru = File.join(chef_server_webui_etc_dir, "config.ru")
 
+erchef_url = "http://#{node['chef_server']['erchef']['listen']}"
+erchef_url << ":#{node['chef_server']['erchef']['port']}"
+
 template env_config do
   source "chef-server-webui-config.rb.erb"
   owner "root"
   group "root"
   mode "0644"
-  variables(node['chef_server']['chef-server-webui'].to_hash)
+  variables(
+    :chef_server_url => erchef_url
+  ).merge(node['chef_server']['chef-server-webui'].to_hash)
   notifies :restart, 'service[chef-server-webui]' if should_notify
 end
 

files/chef-server-cookbooks/chef-server/attributes/default.rb
@@ -129,7 +129,6 @@ default['chef_server']['erchef']['s3_url_ttl'] = 900
 default['chef_server']['erchef']['s3_parallel_ops_timeout'] = 5000
 default['chef_server']['erchef']['s3_parallel_ops_fanout'] = 20
 default['chef_server']['erchef']['proxy_user'] = "pivotal"
-default['chef_server']['erchef']['url'] = "http://127.0.0.1:8000"
 default['chef_server']['erchef']['validation_client_name'] = "chef-validator"
 default['chef_server']['erchef']['umask'] = "0022"
 default['chef_server']['erchef']['web_ui_client_name'] = "chef-webui"

files/chef-server-cookbooks/chef-server/recipes/bootstrap.rb
@@ -18,8 +18,11 @@
 bootstrap_status_file = "/var/opt/chef-server/bootstrapped"
 erchef_dir = "/opt/chef-server/embedded/service/erchef"
 
+erchef_status_url = "http://#{node['chef_server']['erchef']['listen']}"
+erchef_status_url << ":#{node['chef_server']['erchef']['port']}/_status"
+
 execute "verify-system-status" do
-  command "curl -sf http://localhost:8000/_status"
+  command "curl -sf #{erchef_status_url}"
   retries 20
   not_if { File.exists?(bootstrap_status_file) }
 end

files/chef-server-cookbooks/chef-server/templates/default/chef-server-webui-config.rb.erb
@@ -36,7 +36,7 @@ ChefServerWebui::Application.configure do
 
   # Environment specific application configuration.
   # These values override the ones set in 'RAILS_ROOT/config/application.rb'
-  config.chef_server_url = "<%= node['chef_server']['erchef']['url'] %>"
+  config.chef_server_url = "<%= @chef_server_url %>"
   config.rest_client_name = "<%= @web_ui_client_name %>"
   config.rest_client_key = "/etc/chef-server/chef-webui.pem"
   config.admin_user_name =  "<%= @web_ui_admin_user_name %>"

chef-boneyard/omnibus-chef

commit fa5069b410cce1d87d278f630a9f0c0a98cdf59c

Fix regression introduced with 9f9cc08aa1
The merge was being performed incorrectly on the variables attribute NOT
the hash going into that attribute.

files/chef-server-cookbooks/chef-server/recipes/chef-server-webui.rb
@@ -50,9 +50,9 @@ template env_config do
   owner "root"
   group "root"
   mode "0644"
-  variables(
+  variables({
     :chef_server_url => erchef_url
-  ).merge(node['chef_server']['chef-server-webui'].to_hash)
+  }.merge(node['chef_server']['chef-server-webui'].to_hash))
   notifies :restart, 'service[chef-server-webui]' if should_notify
 end
 