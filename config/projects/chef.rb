9744ec6abddf4b5364f81fa3523ad60d51512e74
sersut <serdar@opscode.com>
2014-06-05 15:02:12 -0700

    Pin chef to 10.32.2 to test 10.X security release.

--- config/projects/chef.rb
+++ config/projects/chef.rb
@@ -35,7 +35,7 @@ resources_path File.join(files_path, "chef")
 mac_pkg_identifier "com.getchef.pkg.chef"
 
 # You can pin the components to specific versions as below
-override :chef, version: "11.12.8"
+override :chef, version: "10.32.2"
 # override :ohai, version: "7.0.4"
 
 dependency "preparation"
