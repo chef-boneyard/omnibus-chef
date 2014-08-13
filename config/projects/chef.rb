2199a11ad65c45d1d778ee7707704739a885ac12
Scott Hain <shain@getchef.com>
2014-08-13 10:43:41 -0700

    combined windows definitions with regular chef defs

--- config/projects/chef.rb
+++ config/projects/chef.rb
@@ -29,7 +29,11 @@ build_version do
   output_format :git_describe
 end
 
-install_dir    "/opt/chef"
+if windows?
+  install_dir    "c:/opscode/chef"
+else
+  install_dir    "/opt/chef"
+end
 
 mac_pkg_identifier "com.getchef.pkg.chef"
 
@@ -38,3 +42,34 @@ override :rubygems, version: "1.8.29"
 dependency "preparation"
 dependency "chef"
 dependency "version-manifest"
+
+if windows?
+  package_name    "chef-client"
+
+  msi_parameters do
+    # Find path in which chef gem is installed to.
+    # Note that install_dir is something like: c:/opscode/chef
+    search_pattern = "#{install_dir}/**/gems/chef-[0-9]*"
+    chef_gem_path  = Dir.glob(search_pattern).find do |path|
+      File.directory?(path)
+    end
+
+    if chef_gem_path.nil?
+      raise "Could not find a chef gem in `#{search_pattern}'!"
+    end
+
+    # Convert the chef gem path to a relative path based on install_dir
+    relative_path = Pathname.new(chef_gem_path)
+      .relative_path_from(Pathname.new(install_dir))
+      .to_s
+
+    # Return the result as a hash
+    {
+      # We are going to use this path in the startup command of chef
+      # service. So we need to change file seperators to make windows
+      # happy.
+      chef_gem_path: relative_path.gsub(File::SEPARATOR, File::ALT_SEPARATOR),
+      upgrade_code:  'D607A85C-BDFA-4F08-83ED-2ECB4DCD6BC5',
+    }
+  end
+end
--- config/software/chef.rb
+++ config/software/chef.rb
@@ -16,10 +16,20 @@
 
 name "chef"
 
-dependency "ruby"
-dependency "rubygems"
-dependency "libffi"
+
 dependency "bundler"
+
+if windows?
+  dependency "ruby-windows" #includes rubygems
+  dependency "libyaml-windows"
+  dependency "openssl-windows"
+  dependency "ruby-windows-devkit"
+  dependency "cacerts"
+else
+  dependency "ruby"
+  dependency "rubygems"
+  dependency "libffi"
+end
 
 default_version "master"
 
@@ -27,8 +37,15 @@ source :git => "git://github.com/opscode/chef"
 
 relative_path "chef"
 
+if windows?
+  always_build (self.project.name == "chef-windows")
+end
+
+
 build do
-  env = with_standard_compiler_flags(with_embedded_path)
+  if not windows?
+    env = with_standard_compiler_flags(with_embedded_path)
+  end
 
   block do
     if File.exist?("#{project_dir}/chef")
@@ -39,59 +56,102 @@ build do
     end
   end
 
-  # The way we install chef is different between chefdk and chef projects
-  # due to the fact that chefdk project has appbundler enabled.
-  # Two differences are:
-  #   1-) Order of bundle install & rake gem
-  #   2-) "-n #{install_dir}/bin" option for gem install
-  # We don't expect any side effects from (1) other than not creating
-  # link to erubis binary (which is not needed other than ruby 1.8.7 due to
-  # change that switched the template syntax checking to native ruby code.
-  # Not having (2) does not create symlinks for binaries under
-  # #{install_dir}/bin which gets created by appbundler later on.
-  if %w{ chef angrychef chef-container }.include?(project.name)
-    # install chef first so that ohai gets installed into /opt/chef/bin/ohai
-    rake "gem", env: env
+  # big split between windows and non windows installs happens here
+  if windows?
+    # Normally we would symlink the required unix tools.
+    # However with the introduction of git-cache to speed up omnibus builds,
+    # we can't do that anymore since git on windows doesn't support symlinks.
+    # https://groups.google.com/forum/#!topic/msysgit/arTTH5GmHRk
+    # Therefore we copy the tools to the necessary places.
+    # We need tar for 'knife cookbook site install' to function correctly
+    {
+      'tar.exe'          => 'bsdtar.exe',
+      'libarchive-2.dll' => 'libarchive-2.dll',
+      'libexpat-1.dll'   => 'libexpat-1.dll',
+      'liblzma-1.dll'    => 'liblzma-1.dll',
+      'libbz2-2.dll'     => 'libbz2-2.dll',
+      'libz-1.dll'       => 'libz-1.dll',
+    }.each do |target, to|
+      source = "#{install_dir}/embedded/mingw/bin/#{to}"
+      target = "#{install_dir}/bin/#{target}"
+      copy(source, target)
+    end
 
-    delete "pkg/chef-*-x86-mingw32.gem"
+    rake "gem"
 
-    gem "install pkg/chef-*.gem" \
+    gem "install pkg/chef*mingw32.gem" \
         " --bindir '#{install_dir}/bin'" \
-        " --no-ri --no-rdoc", env: env
-
-    # install the whole bundle, so that we get dev gems (like rspec) and can
-    # later test in CI against all the exact gems that we ship (we will run
-    # rspec unbundled in the test phase).
-    bundle "install --without server docgen", env: env
-  else
-    # install the whole bundle first
-    bundle "install --without server docgen", env: env
-
-    rake "gem", env: env
-
-    delete "pkg/chef-*-x86-mingw32.gem"
-
-    # Don't use -n #{install_dir}/bin. Appbundler will take care of them later
-    gem "install pkg/chef-*.gem " \
-        " --no-ri --no-rdoc", env: env
-  end
-
-  auxiliary_gems = {}
-  auxiliary_gems['ruby-shadow'] = '>= 0.0.0' unless Ohai['platform'] == 'aix'
-
-  auxiliary_gems.each do |name, version|
-    gem "install #{name}" \
-        " --version '#{version}'" \
         " --no-ri --no-rdoc" \
-        " --verbose", env: env
-  end
+        " --verbose"
 
-  # Clean up
-  delete("#{install_dir}/embedded/docs")
-  delete("#{install_dir}/embedded/share/man")
-  delete("#{install_dir}/embedded/share/doc")
-  delete("#{install_dir}/embedded/share/gtk-doc")
-  delete("#{install_dir}/embedded/ssl/man")
-  delete("#{install_dir}/embedded/man")
-  delete("#{install_dir}/embedded/info")
+    # Depending on which shell is being used, the path environment variable can
+    # be "PATH" or "Path". If *both* are set, only one is honored.
+    path_key = ENV.keys.grep(/\Apath\Z/i).first
+
+    bundle "install", env: {
+      path_key => [
+        windows_safe_path(install_dir, 'embedded', 'bin'),
+        windows_safe_path(install_dir, 'embedded', 'mingw', 'bin'),
+        windows_safe_path('C:/Windows/system32'),
+        windows_safe_path('C:/Windows'),
+        windows_safe_path('C:/Windows/System32/Wbem'),
+      ].join(File::PATH_SEPARATOR)
+    }
+  else
+    # The way we install chef is different between chefdk and chef projects
+    # due to the fact that chefdk project has appbundler enabled.
+    # Two differences are:
+    #   1-) Order of bundle install & rake gem
+    #   2-) "-n #{install_dir}/bin" option for gem install
+    # We don't expect any side effects from (1) other than not creating
+    # link to erubis binary (which is not needed other than ruby 1.8.7 due to
+    # change that switched the template syntax checking to native ruby code.
+    # Not having (2) does not create symlinks for binaries under
+    # #{install_dir}/bin which gets created by appbundler later on.
+    if %w{ chef angrychef chef-container }.include?(project.name)
+      # install chef first so that ohai gets installed into /opt/chef/bin/ohai
+      rake "gem", env: env
+
+      delete "pkg/chef-*-x86-mingw32.gem"
+
+      gem "install pkg/chef-*.gem" \
+          " --bindir '#{install_dir}/bin'" \
+          " --no-ri --no-rdoc", env: env
+
+      # install the whole bundle, so that we get dev gems (like rspec) and can
+      # later test in CI against all the exact gems that we ship (we will run
+      # rspec unbundled in the test phase).
+      bundle "install --without server docgen", env: env
+    else
+      # install the whole bundle first
+      bundle "install --without server docgen", env: env
+
+      rake "gem", env: env
+
+      delete "pkg/chef-*-x86-mingw32.gem"
+
+      # Don't use -n #{install_dir}/bin. Appbundler will take care of them later
+      gem "install pkg/chef-*.gem " \
+          " --no-ri --no-rdoc", env: env
+    end
+
+    auxiliary_gems = {}
+    auxiliary_gems['ruby-shadow'] = '>= 0.0.0' unless Ohai['platform'] == 'aix'
+
+    auxiliary_gems.each do |name, version|
+      gem "install #{name}" \
+          " --version '#{version}'" \
+          " --no-ri --no-rdoc" \
+          " --verbose", env: env
+    end
+
+    # Clean up
+    delete("#{install_dir}/embedded/docs")
+    delete("#{install_dir}/embedded/share/man")
+    delete("#{install_dir}/embedded/share/doc")
+    delete("#{install_dir}/embedded/share/gtk-doc")
+    delete("#{install_dir}/embedded/ssl/man")
+    delete("#{install_dir}/embedded/man")
+    delete("#{install_dir}/embedded/info")
+  end
 end
