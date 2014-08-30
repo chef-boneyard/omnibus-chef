27db485e18cdebac33eccdb1c73f4a4325de6320
adamedx <adamed@getchef.com>
2014-08-30 16:42:35 -0700

    Switch to adamedx/windows_paths_leading

--- config/projects/chef-windows.rb
+++ config/projects/chef-windows.rb
@@ -39,7 +39,7 @@ package_name    "chef-client"
 override :bundler,  version: "1.7.0"
 override :ruby,     version: "2.1.2"
 override :rubygems, version: "2.2.1"
-override :"chef-windows", version: 'adamedx/windows_paths'
+override :"chef-windows", version: 'adamedx/windows_paths_leading'
 override :ohai, version: '7.4.0.rc.0'
 
 dependency "preparation"
