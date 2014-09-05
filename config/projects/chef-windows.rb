7fc9d4d40e424ca7a49210b89ea3df7cf037fdd9
adamedx <adamed@getchef.com>
2014-09-05 04:19:51 -0700

    Update for Chef 11.16.0.rc.1 release

--- config/projects/chef-windows.rb
+++ config/projects/chef-windows.rb
@@ -39,7 +39,7 @@ package_name    "chef-client"
 override :rubygems, version: "1.8.29"
 
 # Override chef version for release
-override :"chef-windows", version: "11.16.0.rc.0"
+override :"chef-windows", version: "11.16.0.rc.1"
 
 dependency "preparation"
 dependency "chef-windows"
