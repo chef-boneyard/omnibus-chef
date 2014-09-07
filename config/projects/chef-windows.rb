0ec4f61c96e13879325d15f9fb2bfa03ed9eb209
adamedx <adamed@getchef.com>
2014-09-06 23:13:09 -0700

    Update for Chef 11.16.0 release

--- config/projects/chef-windows.rb
+++ config/projects/chef-windows.rb
@@ -39,7 +39,7 @@ package_name    "chef-client"
 override :rubygems, version: "1.8.29"
 
 # Override chef version for release
-override :"chef-windows", version: "11.16.0.rc.2"
+override :"chef-windows", version: "11.16.0"
 
 dependency "preparation"
 dependency "chef-windows"
--- config/projects/chef.rb
+++ config/projects/chef.rb
@@ -37,7 +37,7 @@ mac_pkg_identifier "com.getchef.pkg.chef"
 override :rubygems, version: "1.8.29"
 
 # Override chef version for release
-override :chef, version: "11.16.0.rc.2"
+override :chef, version: "11.16.0"
 
 dependency "preparation"
 dependency "chef"
