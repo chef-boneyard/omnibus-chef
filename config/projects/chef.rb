f2489dcea199ae2b6fe691cb47ae716e91f21a53
adamedx <adamed@getchef.com>
2014-09-24 21:40:58 -0700

    Ohai override for ffi 1.9.4 bug

--- config/projects/chef.rb
+++ config/projects/chef.rb
@@ -45,6 +45,7 @@ override :'ruby-windows', version: "2.0.0-p451"
 override :rubygems,       version: "2.4.1"
 
 # adamedx build
+override :ohai, version: "adamedx/ffiyajl-workaround"
 override :chef, version: "adamedx/ffiyajl-workaround"
 
 dependency "preparation"
