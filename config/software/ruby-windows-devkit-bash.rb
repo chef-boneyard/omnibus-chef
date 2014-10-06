586fa0b8ce5b242a90d00d2dba04958ac7580d58
Jay Mundrawala <jdmundrawala@gmail.com>
2014-10-06 14:17:42 -0700

    Added patched bash

---
+++ config/software/ruby-windows-devkit-bash.rb
@@ -0,0 +1,41 @@
+#
+# Copyright 2014 Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name "ruby-windows-devkit-bash"
+default_version "3.1.22-4-msys-1.0.18"
+
+dependency "ruby-windows-devkit"
+
+source :url => "https://github.com/jdmundrawala/bash-test/releases/download/bash-#{version}/bash-#{version}-bin.tar.lzma",
+       :md5 => "7182920ebb3fd81f80defd550a8380af"
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  tmpdir = File.join(Omnibus::Config.cache_dir, "openssl-cache")
+  mkdir tmpdir
+
+  # First extract the tar file out of lzma archive.
+  command "7z.exe x #{project_file} -o#{tmpdir} -r -y", env: env
+
+  # Now extract the files out of tar archive.
+  command "7z.exe x #{File.join(tmpdir, "bash-#{version}-bin.tar")} -o#{tmpdir} -r -y", env: env
+
+  # Copy over the required exes into embedded/bin
+  copy "#{tmpdir}/bin/bash.exe", "#{install_dir}/embedded/bin/"
+  copy "#{tmpdir}/bin/sh.exe", "#{install_dir}/embedded/bin/"
+  copy "#{tmpdir}/bin/basbug", "#{install_dir}/embedded/bin/"
+end
