c761cd1e3b259cc4e5e5798367e796f594f6f488
Chris Doherty <cdoherty@getchef.com>
2014-10-08 14:59:10 -0700

    fiddling with directory ownerships.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,14 @@
-git "/home/vagrant/chef" do
-  repository "git://github.com/opscode/chef.git"
-  depth 1
-  user "vagrant"
+%w{chef rubies}.each do |opt_dir|
+  directory "/opt/#{opt_dir} is vagrant:vagrant" do
+    path "/opt/#{opt_dir}"
+    owner "vagrant"
+    group "vagrant"
+    recursive true
+  end
 end
+
+# git "/home/vagrant/chef" do
+#   repository "git://github.com/opscode/chef.git"
+#   depth 1
+#   user "vagrant"
+# end
