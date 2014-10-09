2f28916bebd063c54d16f131aecd340c3d14edde
Chris Doherty <cdoherty@getchef.com>
2014-10-08 18:58:18 -0700

    broken checkpoint.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,5 @@
 # terrible hack because the omnibus-chef build tries to write into /opt/rubies.
-%w{/opt/chef /opt/rubies}.each do |dir|
+%w{ /opt/chef /opt/rubies }.each do |dir|
   execute "chown -R vagrant:vagrant #{dir}" do
     only_if { ::Dir.exists?(dir) }
   end
@@ -9,8 +9,7 @@ BASE    = "/home/vagrant"
 PKG_DIR = "#{BASE}/omnibus-chef/pkg"
 SRC_DIR = "#{BASE}/chef"
 
-do the omnibus-chef build from /home/vagrant/chef instead of github.
-bash "omnibus-chef package build from dev checkout" do
+bash "build-chef-package" do
   user "vagrant"
   group "vagrant"
   cwd "#{BASE}/omnibus-chef"
@@ -24,17 +23,49 @@ bash "omnibus-chef package build from dev checkout" do
   EOS
 end
 
-require 'json'
-json_file = Dir.glob("#{PKG_DIR}/*.json")[0]
-pkg_file = JSON.load(File.open(json_file).read)["basename"]
+ruby_block "store-package-info" do
+  block do
+    require 'json'
+    globbed = Dir.glob("#{PKG_DIR}/*.json")
 
-# install the built package.
-bash "install via package file #{pkg_file}" do
-  code "sudo dpkg -i #{PKG_DIR}/#{pkg_file}"
+    pkg_info = if globbed.size > 0
+      json_file = "#{PKG_DIR}/" + globbed[0]
+      JSON.load(File.open(json_file).read)
+    else
+      {}
+    end
 
-  # not_if installed package is same as this file's package.
+    pkg_info["package_fullpath"] = "#{PKG_DIR}/#{pkg_info["basename"]}"
+    node['chef-client-test']['pkg_info'] = pkg_info
+  end
+  action :run
 end
 
-# make sure /opt/chef etc. are first in PATH.
+# # install the built package.
+# bash "install via package file #{pkg_file}" do
+#   code "sudo dpkg -i #{PKG_DIR}/#{pkg_file}"
+
+#   # not_if installed package is same as this file's package.
+# end
+
+package "chef" do
+  notifies :run, "ruby_block[store-package-info]", :immediately
+  action :upgrade
+  # version node['chef-client-test']['pkg_info']["version"]
+  source node['chef-client-test']['pkg_info']["package_fullpath"]
+end
+
 
 # run the specs in /home/vagrant/chef.
+# %w{ unit functional integration }.each do |spec_type|
+%w{  }.each do |spec_type|
+  bash "run the #{spec_type} specs in #{SRC_DIR}/spec/#{spec_type}" do
+    user "vagrant"
+    group "vagrant"
+    cwd SRC_DIR
+    code <<-EOS
+    bundle install
+    bundle exec rspec spec/#{spec_type}
+    EOS
+  end
+end
