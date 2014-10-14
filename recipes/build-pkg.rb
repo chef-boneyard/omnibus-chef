f9176fe78041af7a8203454815d126e1fd0497a4
Chris Doherty <cdoherty@getchef.com>
2014-10-14 11:39:32 -0700

    Use the jenkins build script, so we're not reinventing the wheel. Overwrite /etc/sudoers and set some environment variables.

--- recipes/build-pkg.rb
+++ recipes/build-pkg.rb
@@ -1,9 +1,24 @@
 
 # terrible hack because the omnibus-chef build tries to write into /opt/rubies.
-%w{/opt/chef /opt/rubies}.each do |dir|
-  execute "chown -R vagrant #{dir}" do
-    only_if { ::Dir.exists?(dir) }
-  end
+# %w{/opt/chef /opt/rubies}.each do |dir|
+#   execute "chown -R vagrant #{dir}" do
+#     only_if { ::Dir.exists?(dir) }
+#   end
+# end
+
+# sudo "overwrite-sudoers" do
+  # node.default['authorization']['sudo'] = {
+  #   'groups' => ['sudo', 'admin'],
+  #   'users'  => ['vagrant'],
+  #   'passwordless' => true,
+  # }
+# end
+
+file "/etc/sudoers" do
+  owner "root"
+  group "root"
+  mode '0440'
+  content File.open("#{node.default['client-test']['omnichef_dir']}/jenkins/sudoers").read
 end
 
 bash "build-omnibus-chef" do
@@ -12,14 +27,16 @@ bash "build-omnibus-chef" do
   cwd node.default['client-test']['omnichef_dir']
 
   ENV['USE_LOCAL_CHEF'] ||= "/home/vagrant/chef"
+  ENV['OMNIBUS_PROJECT_NAME'] = "chef"
+  ENV['OVERRIDE_BUILD_USER'] = "vagrant"
 
   # these first 2 steps should be doable with resources.
   code <<-EOS
   env > lastrun-env.sh
+  export BUILD_TAG=`git log --oneline -n1 | awk '{print $1}'`
+  export BUILD_ID=`date '+%Y-%m-%d_%H-%m-%S'`
 
-  bundle install
-  bundle exec omnibus clean chef --purge
-  bundle exec omnibus build chef --config omnibus-client-test.rb
+  #{node.default['client-test']['omnichef_dir']}/jenkins/build
   EOS
 
   not_if { File.exists?("#{node.default['client-test']['omnichef_dir']}/nobuild") }
