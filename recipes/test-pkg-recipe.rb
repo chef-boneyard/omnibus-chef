ba0311c91f4c5a0bc797942f9b08b0771ae7aa09
Chris Doherty <cdoherty@getchef.com>
2014-11-03 17:52:03 -0800

    still broken

--- recipes/test-pkg-recipe.rb
+++ recipes/test-pkg-recipe.rb
@@ -11,19 +11,34 @@ else
       end
 
       node.run_state[:client_test][:symlinks] = %w{chef-client chef-solo chef-apply chef-shell knife shef ohai}
+
+      pkg_provider, extension = value_for_platform_family(
+                                  "debian"                   => [ Chef::Provider::Package::Dpkg, "deb" ],
+                                  ["rhel", "fedora", "arch"] => [ Chef::Provider::Package::Yum, "rpm" ],
+                                )
+
+      pkg_file = Dir.glob("#{default['client-test']['omnichef_dir']}/pkg/*.#{extension}").
+                sort_by{ |f| File.mtime(f) }.reverse[0]
+
+      if pkg_file.nil?
+        raise Chef::Exception.new "Could not find a candidate package file in pkg/*.#{extension}; did you build one?"
+      end
+
+      { provider: pkg_provider, extension: extension, pkg_file: pkg_file }.each do |k, v|
+        node.run_state[:client_test][k] = v
+      end
+
+      Chef::Log.info "#{pkg_provider} #{extension} #{pkg_file}"
     end
   end
 
-  ruby_block "create-pkg-cmds" do
-    block do
-    end
-  end
-
+  # :purge is not supported by all providers, but it would be nice to use when it is.
   package "uninstall-previous-chef" do
     package_name "chef"
     action :remove
   end
 
+  # TODO: loop this with file resources.
   bash "nuke-old-remnants" do
     code <<-EOS
     rm -rf /opt/chef/*
@@ -43,13 +58,16 @@ else
 
     ENV['PATH'] = "/opt/chef/bin:/opt/chef/embedded/bin:ENV['PATH']"
 
-    # think PATH here is redundant since we're not sudoing.
+    # I think PATH here is redundant since we're not sudoing.
     code <<-EOS
     env PATH=#{ENV['PATH']} TERM=xterm bundle exec rspec -r rspec_junit_formatter \
-    -f RspecJunitFormatter -o $WORKSPACE/test.xml -f documentation spec
+    -f RspecJunitFormatter -o #{ENV['TEMP']}/test.xml -f documentation spec
     EOS
   end
 
-  # uninstall the built package.
-
+  # uninstall the built package--meant to test postrm scripts?
+  package "uninstall-tested-chef" do
+    package_name "chef"
+    action :remove
+  end
 end
