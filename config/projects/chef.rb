124ec47ecb2353cf8e2e3bb33c32d477d548e2a5
Jay Mundrawala <jdmundrawala@gmail.com>
2015-01-13 13:32:06 -0800

    Point to chef 11.18.0

--- config/projects/chef.rb
+++ config/projects/chef.rb
@@ -40,7 +40,7 @@ override :rubygems, version: "2.1.11"
 override :bundler,  version: "1.7.5"
 
 # Override chef version for release
-override :chef, version: "11.16.4"
+override :chef, version: "11.18.0"
 
 dependency "preparation"
 dependency "chef"
