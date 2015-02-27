e717f2d76e6005f72f1d598b1d674730299ee7a3
Jay Mundrawala <jdmundrawala@gmail.com>
2015-02-26 19:17:53 -0800

    Testing my win-service branch

--- config/projects/chef.rb
+++ config/projects/chef.rb
@@ -61,6 +61,7 @@ override :ruby,           version: "2.1.4"
 override :'ruby-windows', version: "2.0.0-p451"
 ######
 override :rubygems,       version: "2.4.4"
+override :chef, version: 'jdm/win-service'
 
 dependency "preparation"
 dependency "chef"
