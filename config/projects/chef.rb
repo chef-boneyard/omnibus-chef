8d03b4b1eceb95701672ae95e03a0058b16d9366
Jay Mundrawala <jdmundrawala@gmail.com>
2015-03-25 17:07:56 -0700

    Chef 12.2.0.rc.2 version bump

--- config/projects/chef.rb
+++ config/projects/chef.rb
@@ -66,8 +66,8 @@ override :'ruby-windows', version: "2.0.0-p451"
 override :rubygems,       version: "2.4.4"
 
 # Chef Release version pinning
-override :chef, version: "12.2.0.rc.1"
-override :ohai, version: "8.1.1"
+override :chef, version: "12.2.0.rc.2"
+override :ohai, version: "8.2.0"
 
 dependency "preparation"
 dependency "chef"
