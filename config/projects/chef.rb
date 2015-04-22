d2a62dd0dce6a970339f0e5971cc6d0693c5d31a
Thom May <thom@may.lt>
2015-04-22 09:40:27 +0100

    pin to a specific 12.3.0.rc.0 tag

--- config/projects/chef.rb
+++ config/projects/chef.rb
@@ -63,7 +63,7 @@ override :'ruby-windows', version: "2.0.0-p451"
 override :rubygems,       version: "2.4.4"
 
 # Chef Release version pinning
-override :chef, version: "12.3-stable"
+override :chef, version: "12.3.0.rc.0"
 override :ohai, version: "tm/8.3.0"
 
 dependency "preparation"
