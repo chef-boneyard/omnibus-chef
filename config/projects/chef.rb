3c6e776f2c72d1f5a9d59fe184b7fbdfffae937d
Lamont Granquist <lamont@scriptkiddie.org>
2015-05-07 16:16:46 -0700

    i kinda hate this API

--- config/projects/chef.rb
+++ config/projects/chef.rb
@@ -63,7 +63,7 @@ override :'ruby-windows', version: "2.0.0-p451"
 override :rubygems,       version: "2.4.4"
 
 # Chef Release version pinning
-# override :chef, version: "12.3.0"
+ override :chef, version: "lcg/fix-shell-spec-timing"
 # override :ohai, version: "8.3.0"
 
 dependency "preparation"
