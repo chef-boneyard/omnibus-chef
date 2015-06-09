4d9ae7c906bd42e689290e5ddeb3e0b92dfb8db1
Thom May <thom@may.lt>
2015-06-09 11:40:24 +0100

    chef 12.4.0.rc.1

--- config/projects/chef.rb
+++ config/projects/chef.rb
@@ -63,7 +63,7 @@ override :'ruby-windows', version: "2.0.0-p451"
 override :rubygems,       version: "2.4.4"
 
 # Chef Release version pinning
-override :chef, version: "12.4.0.rc.0"
+override :chef, version: "12.4.0.rc.1"
 override :ohai, version: "8.4.0"
 
 dependency "preparation"
