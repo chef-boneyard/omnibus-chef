ba39d0b221885018c1ef092f174a30f10f73c5f4
Mark Anderson <mark@chef.io>
2015-08-03 16:31:42 -0700

    Update to 2.0.0-alpha.6

--- config/software/opscode-pushy-client.rb
+++ config/software/opscode-pushy-client.rb
@@ -17,7 +17,7 @@
 
 name "opscode-pushy-client"
 
-default_version "master"
+default_version "1527e289e4c28f8f0fc0f7b7cc25df75b33902a5"
 
 source git: "git://github.com/opscode/opscode-pushy-client"
 
