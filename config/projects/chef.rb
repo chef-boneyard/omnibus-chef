86613d4971a98c6baac8453baab0d5616ee75113
Irving Popovetsky <irving@getchef.com>
2015-09-01 14:44:33 -0700

    Update rubygems to 2.4.7 to resolve CVE-2015-3900

--- config/projects/chef.rb
+++ config/projects/chef.rb
@@ -42,7 +42,7 @@ override :bundler,        version: "1.7.2"
 override :ruby,           version: "2.1.6"
 
 override :'ruby-windows', version: "2.0.0-p645"
-override :rubygems,       version: "2.4.4"
+override :rubygems,       version: "2.4.7"
 
 # Chef Release version pinning
 override :chef, version: ENV['CHEF_VERSION'] || "master"
