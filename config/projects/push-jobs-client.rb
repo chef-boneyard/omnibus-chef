9ec126ec23ef090723b5206a575757726af1845c
Mark Anderson <mark@chef.io>
2015-09-15 15:05:43 -0700

    Bump push client to 1.3.3

--- config/projects/push-jobs-client.rb
+++ config/projects/push-jobs-client.rb
@@ -24,7 +24,7 @@ replace  "opscode-push-jobs-client"
 conflict "opscode-push-jobs-client"
 
 build_iteration 1
-build_version "1.3.2-rc.0"
+build_version "1.3.3"
 
 if windows?
   # NOTE: Ruby DevKit fundamentally CANNOT be installed into "Program Files"
