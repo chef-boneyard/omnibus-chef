23728b7263d37f978a5ab003d1c078a4b9be0f14
John Keiser <john@johnkeiser.com>
2015-11-09 08:19:34 -0800

    Fix compile/converge issue

--- config/software/chefdk.rb
+++ config/software/chefdk.rb
@@ -81,25 +81,23 @@ build do
 
   # FROM chef
   # Always deploy the powershell modules in the correct place.
-  if windows?
-    mkdir "#{install_dir}/modules/chef"
-    block do
+  block do
+    if windows?
+      mkdir "#{install_dir}/modules/chef"
       copy "#{gem_path('chef-[0-9]*')}/distro/powershell/chef/*", "#{install_dir}/modules/chef"
     end
-  end
 
-  #
-  # FROM chef: install ruby-shadow
-  #
-  unless aix? || windows?
-    gem "install ruby-shadow --no-ri --no-rdoc --verbose", env: env
-  end
+    #
+    # FROM chef: install ruby-shadow
+    #
+    unless aix? || windows?
+      gem "install ruby-shadow --no-ri --no-rdoc --verbose", env: env
+    end
 
-  #
-  # Go through all gems installed via git, and install them directly so that we
-  # can get rid of the git checkouts
-  #
-  block do
+    #
+    # Go through all gems installed via git, and install them directly so that we
+    # can get rid of the git checkouts
+    #
     Dir[ "#{install_dir}/embedded/lib/ruby/gems/*/bundler/gems/*" ].each do |gem_path|
       gem_name = File.basename(gem_path).rpartition("-")[0]
       gemspec_path = "#{gem_path}/#{gem_name}.gemspec"
@@ -111,35 +109,35 @@ build do
         end
       end
       gem "build #{gemspec_path}", cwd: gem_path, env: env
-      gem "install --no-ri --no-rdoc" \
+      gem "install --no-ri --no-rdoc " \
           "--local --ignore-dependencies *.gem", cwd: gem_path, env: env
     end
-  end
 
-  #
-  # Appbundle all the things
-  #
-  # To do this, we go into each app, call bundle install --local to lock it to
-  # the ChefDK's dependencies.
-  #
-  # TODO add insspec. But beware, it wants rubocop 0.32 (might not truly be a hard
-  # requirement, but it's in the Gemfile).
-  #
-  %w(chef berkshelf chef-dk chef-vault test-kitchen
-     foodcritic test-kitchen chefspec fauxhai rubocop
-     knife-spork winrm-transport).each do |gem_name|
-    appbundle "chef-dk/omnibus.gemfile.lock", name: gem_name, env: env
-  end
+    #
+    # Appbundle all the things
+    #
+    # To do this, we go into each app, call bundle install --local to lock it to
+    # the ChefDK's dependencies.
+    #
+    # TODO add insspec. But beware, it wants rubocop 0.32 (might not truly be a hard
+    # requirement, but it's in the Gemfile).
+    #
+    %w(chef berkshelf chef-dk chef-vault test-kitchen
+       foodcritic test-kitchen chefspec fauxhai rubocop
+       knife-spork winrm-transport).each do |gem_name|
+      appbundle "chef-dk/omnibus.gemfile.lock", name: gem_name, env: env
+    end
 
-  # FROM chef
-  # Clean up
-  delete "#{install_dir}/embedded/docs"
-  delete "#{install_dir}/embedded/share/man"
-  delete "#{install_dir}/embedded/share/doc"
-  delete "#{install_dir}/embedded/share/gtk-doc"
-  delete "#{install_dir}/embedded/ssl/man"
-  delete "#{install_dir}/embedded/man"
-  delete "#{install_dir}/embedded/info"
-  delete "#{install_dir}/embedded/lib/ruby/gems/*/cache"
-  delete "#{install_dir}/embedded/lib/ruby/gems/*/bundler/gems"
+    # FROM chef
+    # Clean up
+    delete "#{install_dir}/embedded/docs"
+    delete "#{install_dir}/embedded/share/man"
+    delete "#{install_dir}/embedded/share/doc"
+    delete "#{install_dir}/embedded/share/gtk-doc"
+    delete "#{install_dir}/embedded/ssl/man"
+    delete "#{install_dir}/embedded/man"
+    delete "#{install_dir}/embedded/info"
+    delete "#{install_dir}/embedded/lib/ruby/gems/*/cache"
+    delete "#{install_dir}/embedded/lib/ruby/gems/*/bundler/gems"
+  end
 end
